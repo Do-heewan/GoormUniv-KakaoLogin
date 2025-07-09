86456d00581ebb9e16de843fd135f7310db0c5f0
todotoday <s260822@naver.com>
2025-07-09 11:51:57 +0900

    frontend 코드 추가

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import LoginPage from './page/LoginPage';
+import ForgetPage from './page/ForgetPage';
+import Redirection from './page/Redirection';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import LoginSuccess from './page/LoginSuccess';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage />,
+    errorElement: <div>오류 페이지</div>,
+  },
+  {
+    path: '/forget',
+    element: <ForgetPage />,
+  },
+  {
+    path: '/redirect',
+    element: <Redirection />,
+  },
+  {
+    path: '/good',
+    element: <LoginSuccess />,
+  },
+]);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ frontend/src/page/ForgetPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ForgetPage = () => {
+  return <div>여기서 찾아보세요 ㅋㅋ</div>;
+};
+
+export default ForgetPage;
