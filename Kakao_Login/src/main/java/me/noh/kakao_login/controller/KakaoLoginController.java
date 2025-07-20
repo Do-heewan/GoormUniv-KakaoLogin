e860daece2f8846928cd53b093b7f92949ce4e1e
Noh <nhw3152@gmail.com>
2025-07-20 16:52:25 +0900

    로그인 사용자 정보 반환
    client_secret 키 추가

--- Kakao_Login/src/main/java/me/noh/kakao_login/controller/KakaoLoginController.java
+++ Kakao_Login/src/main/java/me/noh/kakao_login/controller/KakaoLoginController.java
@@ -1,7 +1,11 @@
 package me.noh.kakao_login.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.noh.kakao_login.service.KakaoLoginService;
+import me.noh.kakao_login.service.KakaoUserInfoResponseDto;
+import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +17,21 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "https://groom-kakao-login.netlify.app")
 public class KakaoLoginController {
 
+    final KakaoLoginService kakaoLoginService;
+
     @GetMapping("/redirect")
     public ResponseEntity<String> redirect(@RequestParam("code") String code) {
+        // System.out.println(code);
+        String token = kakaoLoginService.getAccessTokenFromKakao(code); // code로 token 발급
+
+        KakaoUserInfoResponseDto userInfo = kakaoLoginService.getUserInfo(token); // token으로 user Info 불러오기
+
+        Long userId = userInfo.getId();
+        String username = userInfo.getKakaoAccount().getProfile().getNickName();
+        String userEmail = userInfo.getKakaoAccount().getEmail();
+
+        // System.out.println(userId + username + userEmail);
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- Kakao_Login/src/main/java/me/noh/kakao_login/service/KakaoLoginService.java
+++ Kakao_Login/src/main/java/me/noh/kakao_login/service/KakaoLoginService.java
@@ -12,16 +12,17 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
 public class KakaoLoginService {
     private String clientId;
+    private String clientSecret;
     private final String KAUTH_TOKEN_URL_HOST;
     private final String KAUTH_USER_URL_HOST;
 
     @Autowired
-    public KakaoLoginService(@Value("${kakao.client_id}") String clientId) {
+    public KakaoLoginService(@Value("${kakao.client_id}") String clientId, @Value("${kakao.client_secret}") String clientSecret) {
         this.clientId = clientId;
+        this.clientSecret = clientSecret;
         KAUTH_TOKEN_URL_HOST ="https://kauth.kakao.com";
         KAUTH_USER_URL_HOST = "https://kapi.kakao.com";
     }
@@ -35,6 +36,7 @@ public class KakaoLoginService {
                         .queryParam("grant_type", "authorization_code")
                         .queryParam("client_id", clientId)
                         .queryParam("code", code)
+                        .queryParam("client_secret", clientSecret)
                         .build(true))
                 .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
                 .retrieve()
@@ -45,11 +47,10 @@ public class KakaoLoginService {
                 .block();
 
 
-        log.info(" [Kakao Service] Access Token ------> {}", kakaoTokenResponseDto.getAccessToken());
-        log.info(" [Kakao Service] Refresh Token ------> {}", kakaoTokenResponseDto.getRefreshToken());
-        //제공 조건: OpenID Connect가 활성화 된 앱의 토큰 발급 요청인 경우 또는 scope에 openid를 포함한 추가 항목 동의 받기 요청을 거친 토큰 발급 요청인 경우
-        log.info(" [Kakao Service] Id Token ------> {}", kakaoTokenResponseDto.getIdToken());
-        log.info(" [Kakao Service] Scope ------> {}", kakaoTokenResponseDto.getScope());
+//        log.info(" [Kakao Service] Access Token ------> {}", kakaoTokenResponseDto.getAccessToken());
+//        log.info(" [Kakao Service] Refresh Token ------> {}", kakaoTokenResponseDto.getRefreshToken());
+//        log.info(" [Kakao Service] Id Token ------> {}", kakaoTokenResponseDto.getIdToken());
+//        log.info(" [Kakao Service] Scope ------> {}", kakaoTokenResponseDto.getScope());
 
         return kakaoTokenResponseDto.getAccessToken();
     }
@@ -70,9 +71,9 @@ public class KakaoLoginService {
                 .bodyToMono(KakaoUserInfoResponseDto.class)
                 .block();
 
-        log.info("[ Kakao Service ] Auth ID ---> {} ", userInfo.getId());
-        log.info("[ Kakao Service ] NickName ---> {} ", userInfo.getKakaoAccount().getProfile().getNickName());
-        log.info("[ Kakao Service ] ProfileImageUrl ---> {} ", userInfo.getKakaoAccount().getProfile().getProfileImageUrl());
+//        log.info("[ Kakao Service ] Auth ID ---> {} ", userInfo.getId());
+//        log.info("[ Kakao Service ] NickName ---> {} ", userInfo.getKakaoAccount().getProfile().getNickName());
+//        log.info("[ Kakao Service ] ProfileImageUrl ---> {} ", userInfo.getKakaoAccount().getProfile().getProfileImageUrl());
 
         return userInfo;
     }
