df74ca12ea6a36aa79afc45d97dddeb06400cd1b
Noh <nhw3152@gmail.com>
2025-07-25 12:51:40 +0900

    로그인 유저 정보 반환

--- Kakao_Login/src/main/java/me/noh/kakao_login/controller/KakaoLoginController.java
+++ Kakao_Login/src/main/java/me/noh/kakao_login/controller/KakaoLoginController.java
@@ -3,24 +3,25 @@ package me.noh.kakao_login.controller;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.noh.kakao_login.dto.UserResponseDto;
 import me.noh.kakao_login.service.KakaoLoginService;
 import me.noh.kakao_login.service.KakaoUserInfoResponseDto;
 import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("")
-@CrossOrigin(origins = "https://groom-kakao-login.netlify.app")
 public class KakaoLoginController {
 
     final KakaoLoginService kakaoLoginService;
 
     @GetMapping("/redirect")
-    public ResponseEntity<String> redirect(@RequestParam("code") String code) {
+    public ResponseEntity<KakaoUserInfoResponseDto> redirect(@RequestParam("code") String code) {
         // System.out.println(code);
         String token = kakaoLoginService.getAccessTokenFromKakao(code); // code로 token 발급
 
@@ -30,8 +31,9 @@ public class KakaoLoginController {
         String username = userInfo.getKakaoAccount().getProfile().getNickName();
         String userEmail = userInfo.getKakaoAccount().getEmail();
 
-        // System.out.println(userId + username + userEmail);
+        System.out.println(userInfo);
+        System.out.println(userId + username + userEmail);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok(userInfo);
     }
 }
